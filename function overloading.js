8bd263dcd39d856cf2053c0a2e424ff7ef76808e
Dipalirangpariya <102205330+Dipalirangpariya@users.noreply.github.com>
2022-04-18 13:00:11 +0530

    Add files via upload

---
+++ function overloading.js
@@ -0,0 +1,34 @@
+let n;
+let num;
+function getpattern(n){
+    num = 1;
+    for (var i = 0; i < n; i++)
+    {
+        for (var j = 0; j <= i; j++ )
+        {
+          console.log( "num");
+        }
+            num = num + 1;
+        console.log(" ");
+    }
+}
+function getpattern(n,num){
+
+    for (var i = 0; i < n; i++)
+    {
+  
+        for (var j = 0; j <= i; j++ )
+        {
+              
+         console.log( "num");
+        }
+          
+            num = num + 1;
+  
+        console.log( "\n");
+    }
+}
+
+
+getpattern(5);
+getpattern(5,1);
